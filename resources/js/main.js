e69d79d7fed31118fa07e287c6dfe2a21dde87fb
jpffgch <charliam.he@tsgforce.com>
2015-11-16 18:31:08 -0500

    Added a mini calendar. Stil have to decide what to do with images on navbar top

--- resources/js/main.js
+++ resources/js/main.js
@@ -97,6 +97,44 @@ return {
 };
 }]);
 
+appModule.directive('minical',  ['$rootScope', function($rootScope) {
+return {
+    restrict: 'A',
+    link: function($scope, element) {
+        
+        // Run Code here
+
+        $(element).fullCalendar({
+            header: {},
+
+            googleCalendarApiKey: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs ',
+             
+            // Populates US holidays and Example FFG holidays
+            events: {
+                googleCalendarId: 'onlymakebelieve.org_o8mk4ka4mrbo2101de9mfg9f50@group.calendar.google.com',
+            },
+            eventClick: function(event) {
+                return false;
+            },
+            eventLimit: 1,
+            eventRender: function(event, element, view) {
+                var dateString = moment(event.start).format('YYYY-MM-DD');
+
+                view.el.find('.fc-day[data-date="' + dateString + '"]')
+                    .addClass('fc-highlight');
+            },
+
+            dayRender: function(date, cell) {
+                //cell.addClass("fc-highlight");
+            },
+        }); 
+
+        // End code
+
+    }
+};
+}]);
+
 $(document).ready(function() {
    
    $("#menu-btn").on("click", function() {

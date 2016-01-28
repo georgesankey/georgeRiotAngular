7f4bc7900cc581aa95c27bc56f7c4d8ac6ab37e9
Raihan Ahmed <raymanahmed@gmail.com>
2016-01-28 15:36:20 -0500

    Fixed account request

---
+++ api/accountrequests.php
@@ -0,0 +1,27 @@
+<?php
+
+/* API for obtaining account requests */
+
+// For Dev
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Authenticate use of API:
+require_once __DIR__ . '/../include/db/dbconfig.php';
+require __DIR__ . '/../include/Auth.php';
+require __DIR__ . '/../include/functions/accountrequests.php';
+$auth = new OMBAuth($cfg, $dbh);
+
+if(!$auth->loggedIn()) {
+	die("403 Forbidden");
+}
+
+// Route calls by action
+$returnValue = 'An error has occured';
+
+//$userParam = isset($_GET["user"]) ? $_GET["user"] : null;
+$returnValue = getAllAccountRequests();
+
+exit(json_encode($returnValue));
+
+?>
--- resources/js/controllers/UserController.js
+++
@@ -1,5 +0,0 @@
-appModule.controller('UserController', function($scope, currentUserService) {
-    currentUserService.getUserData().success(function(user){
-            $scope.user = user;
-        });        
-    });

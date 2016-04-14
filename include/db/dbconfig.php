30ebfbf39b86d78843bb77a73a64edd4ed3c1e02
jpffgch <charliam.he@tsgforce.com>
2016-04-14 16:46:54 -0400

    so done with dbconfig conflicts

--- include/db/dbconfig.php
+++ include/db/dbconfig.php
@@ -1,34 +1,26 @@
 <?php
-$db_host = "localhost"; // "localhost:9090" or wherever your mysql server is
-$db_name ="omb_db";
-$username = "root";
-$password = "test";
-//establish a PDO Database Connection
- try {
-	$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
- 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
- } catch (PDOException $e)
-{
-	die('sorry m8 could not connect'. $e);
+
+// Flip this to false after deploy
+$devbox = true;
+
+if($devbox) {
+	require_once(__DIR__."/../../../dbconfig.php");
+} else {
+	$db_host = "mysql51-075.wc1"; // "localhost:9090" or wherever your mysql server is
+	$db_name = "876215_devomb";
+	$username = "876215_devjp";
+	$password = "ti4kzA2akBKXry";
+	//establish a PDO Database Connection
+	try {
+		$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
+
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+
+	} catch (PDOException $e)
+	{
+		die('Connection failed: '. $e);
+	}
 }
-
-/*
-$db_host = "mysql51-075.wc1"; // "localhost:9090" or wherever your mysql server is
-$db_name = "876215_devomb";
-$username = "876215_devjp";
-$password = "ti4kzA2akBKXry";
-//establish a PDO Database Connection
- try {
-	$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
-
- 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
-
- } catch (PDOException $e)
-{
-	die('sorry m8 could not connect'. $e);
-}
-
-?>*/
 
 ?>
 
